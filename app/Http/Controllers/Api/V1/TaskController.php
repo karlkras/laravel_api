7f4f29c791247d43226349e8bc6b645ac56cbc8f
karlkras <karlkras@yahoo.com>
2023-06-19 11:05:46 -0700

    first commit

---
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+
+class TaskController extends Controller {
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): AnonymousResourceCollection {
+        return TaskResource::collection(Task::all());
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request) {
+        $task = Task::create($request->validated());
+
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task): TaskResource {
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task): TaskResource {
+        $task->update($request->validated());
+
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task): Response {
+        $task->delete();
+        return response()->noContent();
+    }
+}
